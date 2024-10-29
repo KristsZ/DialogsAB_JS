a24af14083791d514961fe1b2f18c6819cd6f6d3
KristsZ <krists.zarans@gmail.com>
2024-10-29 18:26:44 +0200

    Class_04_a

---
+++ class_04/scripts/scripttest.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple"];
+document.getElementById("demo").innerHTML = fruits;
+
+function myFunction() {
+  fruits[fruits.length] = "Lemon";
+  document.getElementById("demo").innerHTML = fruits;
+}
