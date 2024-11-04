5409fb4f8297b3e248c810510ffdf3ce817a6851
KristsZ <krists.zarans@gmail.com>
2024-11-04 20:55:11 +0200

    Class_06_a

---
+++ Class_06/Scripts/script_while.js
@@ -0,0 +1,22 @@
+
+let n = 0;
+
+console.log(`Salidzināšanas rezultāts pirms cikla - ${n} < ${3} ir ${n < 3}`);
+
+while (n < 3){
+    n++;
+    console.log(`Salidzināšanas rezultāts ciklā (pēc n pieauguma) - ${n} < ${3} ir ${n < 3}`);
+}
+console.log(`Salidzināšanas rezultāts pēc cikla - ${n} < ${3} ir ${n < 3}`);
+    console.log(n);
+    
+let a= 1, A= 3, b= 1, B = 4;
+while (a <= A){
+    b = 1;
+    while (b <= B){
+        console.log(a+" "+b);
+        b++;
+    }
+    a++;    
+}
+
