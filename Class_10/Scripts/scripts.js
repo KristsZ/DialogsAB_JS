517077a80e15e081cdfe3be6966b1c9879a8e0fe
KristsZ <krists.zarans@gmail.com>
2024-11-26 19:23:09 +0200

    Class_10_a

--- Class_10/Scripts/scripts.js
+++ Class_10/Scripts/scripts.js
@@ -6,14 +6,28 @@ const person = {
   
   //document.getElementById("demo").innerHTML = person;
   //document.getElementById("demo").innerHTML = person["name"] + ", " + person["age"] + ", " + person["city"];
-  
+
+
 let text = "";
   console.log("Teksta rindas saturs pirms cikla for: " +text);
+
+/*
 for (let x in person) {
   console.log(x);
   text += person[x] + " ";
   console.log(text);
 };
 console.log("Teksta rindas saturs pēc cikla for: " +text);
+*/
+
+const values_of_prop_person = Object.values(person);
+for (let x of values_of_prop_person) {
+  console.log(x);
+  text += x + " ";
+  console.log(text);
+};
+console.log("Teksta rindas saturs pēc cikla for: " +text);
 
 document.getElementById("demo").innerHTML = text;
+
+
