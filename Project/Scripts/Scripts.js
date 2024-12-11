16416d6408031f1a935ed97b53c1c859d7f60a62
KristsZ <krists.zarans@gmail.com>
2024-12-11 20:18:37 +0200

    Project_a

--- Project/Scripts/Scripts.js
+++ Project/Scripts/Scripts.js
@@ -38,3 +38,14 @@ close.addEventListener("click", () => modal.classList.remove("show-modal"));
 window.addEventListener("click", (e) =>
   e.target == modal ? modal.classList.remove("show-modal") : false
 );
+
+// Funkcija, kas maina fona attēlu
+function changeBackground() {
+  const randomImage = `url("https://picsum.photos/800/600?random=${Math.floor(Math.random() * 10000)}")`;
+  document.body.style.backgroundImage = randomImage;
+}
+// Maina fona attēlu ik pēc 10 sekundēm
+setInterval(changeBackground, 5000);
+
+// Maina fona attēlu uzreiz pēc lapas ielādes
+changeBackground();
